9923130ddfbeaadf314ed3c26cdb990d125c382e
ahmetkayhancetinkaya <80328379+ahmetkayhancetinkaya@users.noreply.github.com>
2021-05-09 21:07:34 +0300

    Day3 Hw2 added

---
+++ Day3_Hw2/odev3Kodlama/src/odev3Kodlama/Persons/Student.java
@@ -0,0 +1,15 @@
+package odev3Kodlama.Persons;
+
+public class Student extends User {
+
+	private String _schoolName;
+
+	public String getSchoolName() {
+		return _schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		_schoolName = schoolName;
+	}
+
+}
